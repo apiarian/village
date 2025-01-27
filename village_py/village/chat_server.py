e06ef451e821501d5d8024399a78d3057b1a54ab
Al <al@al-py.local>
2025-01-26 21:25:40 -0500

    better messages storage and persistence

--- village_py/village/chat_server.py
+++ village_py/village/chat_server.py
@@ -1,12 +1,15 @@
 import json
 import os
 import time
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 from flask import Flask, jsonify, request
 
 chat_app = Flask(__name__)
 chat_app.secret_key = os.environ["CHAT_FLASK_SECRET_KEY"].encode("utf-8")
+
+
+repository = os.path.expanduser("~/test-repository")
 
 
 class Message(NamedTuple):
@@ -22,18 +25,60 @@ class Message(NamedTuple):
         }
 
 
-messages: list[Message] = [
-    Message(
-        timestamp_microseconds=int(time.time() * 1_000_000),
-        author="test",
-        message="some message",
-    ),
-]
+class MessageList:
+    def __init__(self) -> None:
+        self._count_limit = 100
+        self._time_limit_microseconds = 1_000_000 * 60 * 60
+        self._messages: List[Message] = []
+        self._filename = os.path.join(repository, "messages.json")
+
+    def all(self):
+        yield from self._messages
+
+    def max_timestamp_microseconds(self) -> int:
+        if not self._messages:
+            return 0
+
+        return max(message.timestamp_microseconds for message in self._messages)
+
+    def cleanup(self) -> None:
+        if len(self._messages) > self._count_limit:
+            self._messages = self._messages[-self._count_limit:]
+
+        self._messages = [
+            message
+            for message in self._messages
+            if message.timestamp_microseconds > (
+                (time.time() * 1_000_000) - self._time_limit_microseconds
+            )
+        ]
+
+    def append(self, message: Message) -> None:
+        self._messages.append(message)
+        self.cleanup()
+        self.dump_to_file()
+
+    def dump_to_file(self) -> None:
+        with open(self._filename, "wt") as f:
+            json.dump([m.as_dict() for m in self._messages], f)
+
+    def load_from_file(self) -> None:
+        if not os.path.exists(self._filename):
+            return
+
+        with open(self._filename, "rt") as f:
+            for m in json.load(f):
+                self.append(Message(**m))
+
+
+messages = MessageList()
+messages.load_from_file()
 
 
 @chat_app.route("/messages", methods=["GET"])
 def list_messages():
     global messages
+    messages.cleanup()
 
     try:
         since = int(request.args.get("since_microseconds"))
@@ -43,7 +88,7 @@ def list_messages():
     return jsonify(
         [
             message.as_dict()
-            for message in messages
+            for message in messages.all()
             if message.timestamp_microseconds > since
         ]
     )
@@ -52,6 +97,7 @@ def list_messages():
 @chat_app.route("/add_message", methods=["POST"])
 def add_message():
     global messages
+    messages.cleanup()
 
     data = request.get_json()
 
@@ -60,9 +106,7 @@ def add_message():
 
     timestamp_microseconds = int(time.time() * 1_000_000)
 
-    max_timestamp_microseconds = (
-        max(message.timestamp_microseconds for message in messages) if messages else 0
-    )
+    max_timestamp_microseconds = messages.max_timestamp_microseconds()
     if timestamp_microseconds <= max_timestamp_microseconds:
         raise Exception(
             f"timestamp issue: {timestamp_microseconds} for max {max_timestamp_microseconds}"
@@ -75,8 +119,6 @@ def add_message():
             message=message,
         )
     )
-    if len(messages) > 10:
-        messages = messages[-10:]
 
     try:
         since = int(request.args.get("since_microseconds"))
@@ -86,7 +128,7 @@ def add_message():
     return jsonify(
         [
             message.as_dict()
-            for message in messages
+            for message in messages.all()
             if message.timestamp_microseconds > since
         ]
     )
